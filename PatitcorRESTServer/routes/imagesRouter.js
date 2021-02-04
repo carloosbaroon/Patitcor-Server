1955d3026340555c6e5e941b015ecd147a2e89a7
Carlos Baron <carlostectol@gmail.com>
2021-02-04 10:18:16 -0600

    UploadAdImage

--- PatitcorRESTServer/routes/imagesRouter.js
+++ PatitcorRESTServer/routes/imagesRouter.js
@@ -10,13 +10,35 @@ const storage = multer.diskStorage({
     }
 });
 
+const storagePromo = multer.diskStorage({
+    destination: "./public/promos/",
+    filename: function(req, file, cb){
+        cb(null,file.originalname);
+    }
+});
+
 const upload = multer({
     storage: storage,
     limits:{fileSize: 2500000},
 }).single("myImage");
 
+const uploadPromo = multer({
+    storage: storagePromo,
+    limits:{fileSize: 2500000},
+}).single("myImage");
+
 imagesRouter.post('/', function (req, res) {
-    upload(req, res, function (err) {
+    uploadPromo(req, res, function (err) {
+        console.log("Request file ---", req.file);//Here you get file.
+        /*Now do where ever you want to do*/
+        if(!err) {
+            return res.send(200).end();
+        }
+    })
+})
+
+imagesRouter.post('/promo', function (req, res) {
+    uploadPromo(req, res, function (err) {
         console.log("Request file ---", req.file);//Here you get file.
         /*Now do where ever you want to do*/
         if(!err) {
